3b825ca6d9afbd0d7839fa3eeae878da8f69f4df
Pedro Henrique <pedrogthenrique277@gmail.com>
2024-01-22 10:53:16 -0300

    up

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel}Km por hora`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida Vagabundo!`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brazil'
+if (país == 'Brazil') {
+    console.log('Samba! Brazil, Caipirinha!')
+}
+else {
+    console.log('Gringooo!')
+}
